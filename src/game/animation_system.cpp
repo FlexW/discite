ee37b5f70a4a8246dd06cdf1af0cfc692edc6689
Felix Weilbach <felix.weilbach@t-online.de>
2022-03-31 00:24:55 +0200

    Add animation system

---
+++ src/game/animation_system.cpp
@@ -0,0 +1,72 @@
+#include "animation_system.hpp"
+#include "engine.hpp"
+#include "game_layer.hpp"
+#include "log.hpp"
+#include "profiling.hpp"
+#include "skinned_mesh_component.hpp"
+
+namespace dc
+{
+
+void AnimationSystem::init()
+{
+  const auto game_layer = Engine::instance()->layer_stack()->layer<GameLayer>();
+  if (game_layer)
+  {
+    const auto scene = game_layer->scene();
+    if (scene)
+    {
+      scene_ = scene->get();
+    }
+  }
+}
+
+void AnimationSystem::update(float delta_time)
+{
+  DC_PROFILE_SCOPE("AnimationSystem::update()");
+
+  const auto scene = scene_.lock();
+  if (!scene)
+  {
+    DC_LOG_WARN("Scene is not valid. Will not animate meshes");
+  }
+
+  auto view = scene->all_entities_with<SkinnedMeshComponent>();
+  for (const auto entity : view)
+  {
+    auto &skinned_mesh_component = view.get<SkinnedMeshComponent>(entity);
+
+    const auto &skinned_mesh = skinned_mesh_component.skinned_mesh_;
+    if (!skinned_mesh || !skinned_mesh->is_ready())
+    {
+      continue;
+    }
+
+    skinned_mesh->get()->compute_bone_transforms(delta_time);
+  }
+
+  DC_FAIL("Implement");
+}
+
+void AnimationSystem::render(SceneRenderInfo & /*scene_render_info*/,
+                             ViewRenderInfo & /*view_render_info*/)
+{
+}
+
+bool AnimationSystem::on_event(const Event &event)
+{
+  const auto event_id = event.id();
+  if (event_id == SceneLoadedEvent::id)
+  {
+    on_scene_loaded(dynamic_cast<const SceneLoadedEvent &>(event));
+  }
+
+  return false;
+}
+
+void AnimationSystem::on_scene_loaded(const SceneLoadedEvent &event)
+{
+  scene_ = event.scene_;
+}
+
+} // namespace dc
---
+++ src/game/animation_system.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "frame_data.hpp"
+#include "scene.hpp"
+#include "scene_events.hpp"
+#include "system.hpp"
+
+namespace dc
+{
+
+class AnimationSystem : public System
+{
+public:
+  void init() override;
+  void update(float delta_time) override;
+  void render(SceneRenderInfo &scene_render_info,
+              ViewRenderInfo  &view_render_info) override;
+
+  bool on_event(const Event &event) override;
+
+private:
+  std::weak_ptr<Scene> scene_{};
+
+  void on_scene_loaded(const SceneLoadedEvent &event);
+};
+
+} // namespace dc
